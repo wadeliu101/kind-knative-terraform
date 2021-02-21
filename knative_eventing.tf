6554ac6f8e58785e7a5b5717c4da6ef8a539ea42
劉品均 <grassshrimp@liupinjunde-MacBook-Pro.local>
2021-02-22 00:47:12 +0800

    nats event

--- knative_eventing.tf
+++ knative_eventing.tf
@@ -11,18 +11,42 @@ resource "null_resource" "install_knative_eventing" {
   provisioner "local-exec" {
     command = "kubectl wait pod --timeout=-1s --for=condition=Ready -l '!job-name' -n knative-eventing"
   }
-  provisioner "local-exec" {
-    command = "kubectl apply -f https://github.com/knative/eventing/releases/download/${var.KNATIVE_EVENTING_VERSION}/in-memory-channel.yaml"
-  }
-  provisioner "local-exec" {
-    command = "kubectl wait pod --timeout=-1s --for=condition=Ready -l '!job-name' -n knative-eventing"
-  }
-  provisioner "local-exec" {
-    command = "kubectl apply -f https://github.com/knative/eventing/releases/download/${var.KNATIVE_EVENTING_VERSION}/mt-channel-broker.yaml"
-  }
-  provisioner "local-exec" {
-    command = "kubectl wait pod --timeout=-1s --for=condition=Ready -l '!job-name' -n knative-eventing"
-  }
-
   depends_on = [ null_resource.configure_knative_to_use_kourier ]
+}
+
+resource "helm_release" "nats-streaming" {
+  name       = "nats-stan"
+  repository = "https://nats-io.github.io/k8s/helm/charts/"
+  chart      = "stan"
+  version    = "0.7.4"
+  namespace  = "natss"
+
+  values = [
+    <<EOF
+    stan:
+      clusterID: knative-nats-streaming
+      logging:
+        debug: true
+        trace: true
+    nameOverride: nats-streaming
+    store:
+      volume:
+        storageClass: standard
+    EOF
+  ]
+  
+  create_namespace = true
+
+  depends_on = [ null_resource.install_knative_eventing ]
+}
+
+resource "null_resource" "install_the_nats_streaming_channel" {
+  provisioner "local-exec" {
+    command = "kubectl apply --filename https://github.com/knative-sandbox/eventing-natss/releases/download/${var.KNATIVE_EVENTING_VERSION}/eventing-natss.yaml"
+  }
+  provisioner "local-exec" {
+    command = "kubectl wait deployment --all --timeout=-1s --for=condition=Available -n knative-eventing"
+  }
+  
+  depends_on = [ helm_release.nats-streaming ]
 }
--- variables.tf
+++ variables.tf
@@ -5,5 +5,5 @@ variable "KNATIVE_NET_KOURIER_VERSION" {
   default = "v0.20.0"
 }
 variable "KNATIVE_EVENTING_VERSION" {
-  default = "v0.20.1"
+  default = "v0.20.0"
 }
